0536e6f7d8cb9e5f0f2ea6e3cb7f0ac3f3a24aed
Paulo Souza <101979170+paulobsouza@users.noreply.github.com>
2022-09-01 21:41:27 -0300

    Delete sci directory

--- sci/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server = "localhost";
-$user = "root";
-$password = "";
-$db = "sci";
-
-$connection = mysqli_connect($server, $user, $password, $db);
-?>
--- sci/view/gabinete.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once ('../connection.php');
-include_once ('../header/header.php');
-include_once ('../footer/footer.php');
-?>
